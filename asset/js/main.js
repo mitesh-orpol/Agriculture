fcf9a26138b08742180908cdf9571dcaf70f2380
Mitesh <miteshorpol@gmail.com>
2024-05-14 09:09:34 +0530

    some small change

--- asset/js/main.js
+++ asset/js/main.js
@@ -58,40 +58,7 @@ $(".comming").click(function(){
 });
 
 
-(function () {
-    "use strict";
-  
-    var carousels = function () {
-      $(".owl-carousel1").owlCarousel({
-        loop: true,
-        center: true,
-        margin: 0,
-        responsiveClass: true,
-        nav: false,
-        autoplay: true, // Add this line
-        autoplayTimeout: 3000, // Optionally, set autoplay timeout
-        responsive: {
-          0: {
-            items: 1,
-            nav: false
-          },
-          680: {
-            items: 2,
-            nav: false,
-            loop: false
-          },
-          1000: {
-            items: 3,
-            nav: true
-          }
-        }
-      });
-    };
-  
-    (function ($) {
-      carousels();
-    })(jQuery);
-  })();
+
   
 
 
